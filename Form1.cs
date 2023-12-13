20e6567c822d7944d31e08ef485e83468ca1c7b0
yelenanikitina1625@hotmail.com <yelenanikitina1625@hotmail.com>
2023-12-13 22:35:49 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad6_animyli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        bool timerStartr = false;
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmr.Enabled = true;
+            tmr.Interval = 30;
+
+            if (!timerStartr)
+            {
+                tmr.Enabled = true;
+                timerStartr = true;
+                btnStart.Text = "Стоп";
+            }
+ 
+            else if(pct.Left + pct.Width > this.Width)
+            {//если картинка на краю формы то пр нажатии она возвращается в начальное положение
+                pct.Left = 0;
+                tmr.Enabled = false;
+                timerStartr = false;
+                btnStart.Text = "Старт"; 
+                
+                
+             }
+            
+            else
+                {
+                    tmr.Enabled = false;
+                    timerStartr = false;
+                    btnStart.Text = "Старт";
+                }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            pct.Left = pct.Left + 20;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("zad6_animyli")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("zad6_animyli")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5ce83346-e5fa-47ba-a027-a0296146093a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
